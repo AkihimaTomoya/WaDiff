ba09aea55a9059f2fb6bca898399acbb403aa32c
Akihima Tomoya <79859136+AkihimaTomoya@users.noreply.github.com>
2025-07-27 18:44:16 +0700

    Update train.sh

--- StegaStamp/train.sh
+++ StegaStamp/train.sh
@@ -1,2 +1,2 @@
-python train.py --data_dir ./content/data/fridge dataset \
+python train.py --data_dir ./content/data/chair_dataset \
 --bit_length 48 --image_resolution 128 --num_epochs 100 --cuda 0
