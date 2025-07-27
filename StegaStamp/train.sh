7e074e3256c546e55e184150f349732b9dfc6dc3
Akihima Tomoya <79859136+AkihimaTomoya@users.noreply.github.com>
2025-07-27 18:28:36 +0700

    Update train.sh

--- StegaStamp/train.sh
+++ StegaStamp/train.sh
@@ -1,2 +1,2 @@
-python train.py --data_dir ./data/coco/train2014 \
---bit_length 48 --image_resolution 512 --num_epochs 100 --cuda 0
+python train.py --data_dir ./content/data/fridge dataset \
+--bit_length 48 --image_resolution 128 --num_epochs 100 --cuda 0
