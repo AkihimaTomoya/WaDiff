1bfcadd52221e473ceca2edf625f09946278bec4
Akihima Tomoya <79859136+AkihimaTomoya@users.noreply.github.com>
2025-07-27 18:37:51 +0700

    Update train.sh

--- guided-diffusion/train.sh
+++ guided-diffusion/train.sh
@@ -1,3 +1,5 @@
+export OMPI_ALLOW_RUN_AS_ROOT=1
+export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 MODEL_FLAGS="--wm_length 48 --attention_resolutions 32,16,8 --class_cond False --image_size 256 --num_channels 256 --learn_sigma True --num_head_channels 64 --num_res_blocks 2 --resblock_updown True"
 DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear"
 TRAIN_FLAGS="--lr 1e-4 --batch_size 4"
